eb842d5aa1a1c1c1cd8be7508e3187f7dfa8fbc6
Cyberneticus777 <marosleo1990@gmail.com>
2022-08-18 21:45:45 -0300

    Back-semi-pronto

--- pi/watchdogs/urls.py
+++ pi/watchdogs/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('post/new/', views.WatchdogsCreateView.as_view(),name='post_new'),
     path('post/<slug:slug>/', views.WatchdogsDetailView.as_view(),name='post_detail'),
     path('post/<int:pk>/edit/', views.WatchdogsUpdateView.as_view(),name='post_edit'),
-
+    path('post/<int:pk>/delete/', views.WatchdogsDeleteView.as_view(),name='post_delete'),
 
 ]
--- pi/watchdogs/views.py
+++ pi/watchdogs/views.py
@@ -1,5 +1,9 @@
+#from pyexpat.errors import messages
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView,UpdateView
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from django.views.generic import ListView, DetailView,UpdateView, DeleteView
 from django.views.generic.edit import CreateView
 from . models import Post
 # Create your views here.
@@ -14,12 +18,37 @@ class WatchdogsDetailView(DetailView):
     template_name = 'watchdogs/post_detail.html'
     #context_object_name = 'custom'
 
-class WatchdogsCreateView(CreateView):
+class WatchdogsCreateView(SuccessMessageMixin, CreateView):
     model = Post
     template_name = 'watchdogs/post_new.html'
-    fields = ('autor','titulo','conteudo')
+    fields = ('autor','titulo','slug','conteudo')
+    success_message = "%(field)s - criado com sucesso"
 
-class WatchdogsUpdateView(UpdateView):
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            field=self.object.titulo,
+        )
+
+class WatchdogsUpdateView(SuccessMessageMixin, UpdateView):
     model = Post
     template_name = 'watchdogs/post_edit.html'
     fields = ('titulo','conteudo')
+    success_message = "%(field)s - alterado com sucesso"
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            field=self.object.titulo,
+        )
+
+
+class WatchdogsDeleteView(SuccessMessageMixin, DeleteView):
+    model = Post
+    template_name = 'watchdogs/post_delete.html'
+    success_url = reverse_lazy('home')
+    success_message = "Deletado com sucesso"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request,self.success_message)
+        return super(WatchdogsDeleteView,self).delete(request, *args, **kwargs)
