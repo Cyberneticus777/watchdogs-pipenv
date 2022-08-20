960c8172974005eb1d08f7e18553b8d3bec9d889
Cyberneticus777 <marosleo1990@gmail.com>
2022-08-20 16:55:53 -0300

    cadastro

--- pi/pi/settings.py
+++ pi/pi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'watchdogs',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -113,6 +114,10 @@ TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 
 USE_TZ = True
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
 
 
 # Static files (CSS, JavaScript, Images)
